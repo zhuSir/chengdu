94517314a7cae3d3c6646a4db2c91f168e717c99
zhusir <aizgw2012@163.com>
2019-03-11 11:17:53 +0800

    任务修改

--- cd-admin/src/main/java/cn/gribe/modules/job/task/OrderTask.java
+++ cd-admin/src/main/java/cn/gribe/modules/job/task/OrderTask.java
@@ -33,19 +33,23 @@ public class OrderTask {
 		logger.info("===>>>:开始执行订单状态检测定时任务");
 		//发货后7天订单自动收货转待评价
 		EntityWrapper wrapper7 = new EntityWrapper();
-		wrapper7.like("create_time",DateUtils.format(DateUtils.addDateDays(new Date(),-7),""));
+		String time7 = DateUtils.format(DateUtils.addDateDays(new Date(),-7));
+		wrapper7.like("create_time",time7);
 		wrapper7.eq("state", OrderEntity.STATE_AWAIT_USE);
 		List<OrderEntity> orderList = orderService.selectList(wrapper7);
 		for(OrderEntity order : orderList){
+			logger.info("==>>:待使用七天转待评价订单: "+order.toString());
 			order.setState(OrderEntity.STATE_AWAIT_EVALUATE);
 			orderService.updateById(order);
 		}
 		//15天自动转完成订单
 		EntityWrapper wrapper15 = new EntityWrapper();
-		wrapper15.like("create_time",DateUtils.format(DateUtils.addDateDays(new Date(),-15),""));
+		String time15 = DateUtils.format(DateUtils.addDateDays(new Date(),-15));
+		wrapper15.like("create_time",time15);
 		wrapper15.eq("state", OrderEntity.STATE_AWAIT_EVALUATE);
 		List<OrderEntity> orderList15 = orderService.selectList(wrapper15);
 		for(OrderEntity order : orderList15){
+			logger.info("==>>:待评价十五天转完成订单: "+order.toString());
 			order.setState(OrderEntity.STATE_FINISHED);
 			orderService.updateById(order);
 		}
